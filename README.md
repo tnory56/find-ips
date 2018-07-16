f6da0f0dc87ccb070765e57c0f504a29817927fa
Thomas Norberg <tnorberg@thomasnorberg.com>
2018-07-16 13:34:28 -0400

    Adding batch script

---
+++ README.md
@@ -0,0 +1,9 @@
+# FIND IPs
+
+## Windows batch file 
+
+Make sure to run as administrator 
+
+##Power shell ip Scanner
+
+[PowerShell Network scanner](https://github.com/BornToBeRoot/PowerShell_IPv4NetworkScanner)
---
+++ ping.bat
@@ -0,0 +1,27 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+		echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate. Run as administrator to get full access.
+		pause
+		exit /B 1
+	)
+set RANGE=10.10.11
+echo This is your range: %RANGE% (it might take a while)
+set CURRENTDIRECTORY=%~dp0
+set FILELOCATION=%CURRENTDIRECTORY%ipaddresses.txt
+echo IP addresses will output to: %FILELOCATION%
+break>%FILELOCATION%
+start powershell.exe -noprofile -command "&Get-Content -Path \"%FILELOCATION%\" -Wait"
+REM pause
+
+for /l %%i in (1, 1, 254) do (
+	echo Pinging: %RANGE%.%%i
+	ping -n 1 -w 1 %RANGE%.%%i | FIND /i "Reply">>%FILELOCATION%
+)
